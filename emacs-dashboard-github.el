1994f189a62c3b11f7faf76c610e8b58c11e0963
Robin Mattheussen <robin.mattheussen@gmail.com>
2019-06-03 13:49:04 +0200

    Support for first event-types done

--- emacs-dashboard-github.el
+++ emacs-dashboard-github.el
@@ -6,35 +6,49 @@
 (use-package dash
   :straight t)
 
+(use-package dashboard
+  :straight t)
+
+;;(require 'request)
+;;(require 'dash)
+;;(require 'dashboard)
+;;(require 'dashboard-widgets)
+
+(add-to-list 'dashboard-item-generators  '(github . dashboard-insert-github))
+(add-to-list 'dashboard-items '(github) t)
+
 (defconst dgh--supported-event-types'
   ("CreateEvent"
    "ForkEvent"
-   "IssueCommentEvent"
-   "IssuesEvent"
-   "MemberEvent"
-   "MembershipEvent"
-   "PublicEvent"
-   "PullRequestEvent"
-   "PullRequestReviewEvent"
-   "PullRequestReviewCommentEvent"
-   "ReleaseEvent"
-   "RepositoryEvent"
-   "StarEvent"
+   ;; "IssueCommentEvent"
+   ;; "IssuesEvent"
+   ;; "MemberEvent"
+   ;; "MembershipEvent"
+   ;; "PublicEvent"
+   ;; "PullRequestEvent"
+   ;; "PullRequestReviewEvent"
+   ;; "PullRequestReviewCommentEvent"
+   ;; "ReleaseEvent"
+   ;; "RepositoryEvent"
+   ;; "StarEvent"
    "WatchEvent")
   "Github API event types current supported by the package.")
 
 (defconst dgh-test-create
   '((type  . "CreateEvent")
     (actor . ((login . "Joey")))
-    (repo  . ((name . "example/example-repo")
-	      (url . "https://www.example.org")))))
+    (repo  . ((name . "example/example-repo")))))
 
 (defconst dgh-test-fork
   '((type  . "ForkEvent")
     (actor . ((login . "Joey")))
     (payload . ((forkee . ((full_name . "Joey/example-repo")))))
-    (repo  . ((name . "example/example-repo")
-	      (url . "https://www.example.org")))))
+    (repo  . ((name . "example/example-repo")))))
+
+(defconst dgh-test-watch
+  '((type  . "WatchEvent")
+    (actor . ((login . "Joey")))
+    (repo  . ((name . "example/example-repo")))))
 
 (defvar dashboard-github-user-name "romatthe"
   "Github user name for which to fetch events.")
@@ -48,64 +62,83 @@
   "Check if EVENT is one of the supported event types."
   (-contains? dgh--supported-event-types (assoc-default 'type event)))
 
-(defun dgh--get-events (callback)
+(defun dgh--get-events ()
   "Get public Github events, and execute CALLBACK function."
-  (request
-   dashboard-github-user-events-url
-   :sync t
-   :parser 'json-read
-   :success (cl-function
-             (lambda (&key data &allow-other-keys)
-	       (funcall callback data)))))
-
+  (let
+      ((request-result
+	(request
+	 dashboard-github-user-events-url
+	 :sync t
+	 :parser 'json-read
+	 :success (cl-function (lambda (&key data &allow-other-keys) ())))))
+    (append (request-response-data request-result) nil)))
+	
 (defun dgh--parse-events (events)
   "Parse EVENTS data from events request."
-  (-map 'dgh--parse-events
-	(-filter 'dgh--event-is-supported? events)))
+  (-map #'dgh--parse-event
+	(-filter #'dgh--event-is-supported? events)))
 
 (defun dgh--parse-event (event)
   "Parse a single EVENT."
   (pcase (assoc-default 'type event)
-    ("CreateEvent"                   (message (dgh--parse-create-event event)))
-    ("ForkEvent"                     (message (dgh--parse-fork-event event)))
-    ("IssueCommentEvent"             (message "IssueCommentEvent"))
-    ("IssuesEvent"                   (message "IssuesEvent"))
-    ("MemberEvent"                   (message "MemberEvent"))
-    ("MembershipEvent"               (message "MembershipEvent"))
-    ("PublicEvent"                   (message "PublicEvent"))
-    ("PullRequestEvent"              (message "PullRequestEvent"))
-    ("PullRequestReviewEvent"        (message "PullRequestReviewEvent"))
-    ("PullRequestReviewCommentEvent" (message "PullRequestReviewCommentEvent"))
-    ("ReleaseEvent"                  (message "ReleaseEvent"))
-    ("RepositoryEvent"               (message "RepositoryEvent"))
-    ("StarEvent"                     (message "StarEvent"))
-    ("WatchEvent"                    (message "WatchEvent"))))
+    ("CreateEvent"                   (dgh--parse-create-event event))
+    ("ForkEvent"                     (dgh--parse-fork-event event))
+    ;; ("IssueCommentEvent"             (message "IssueCommentEvent"))
+    ;; ("IssuesEvent"                   (message "IssuesEvent"))
+    ;; ("MemberEvent"                   (message "MemberEvent"))
+    ;; ("MembershipEvent"               (message "MembershipEvent"))
+    ;; ("PublicEvent"                   (message "PublicEvent"))
+    ;; ("PullRequestEvent"              (message "PullRequestEvent"))
+    ;; ("PullRequestReviewEvent"        (message "PullRequestReviewEvent"))
+    ;; ("PullRequestReviewCommentEvent" (message "PullRequestReviewCommentEvent"))
+    ;; ("ReleaseEvent"                  (message "ReleaseEvent"))
+    ;; ("RepositoryEvent"               (message "RepositoryEvent"))
+    ;; ("StarEvent"                     (message "StarEvent"))
+    ("WatchEvent"                     (dgh--parse-watch-event event))))
 
 (defun dgh--parse-create-event (event)
   "Parse a CreateEvent data structure from EVENT."
-  '((msg . (format "%s created a repository %s"
-		   (assoc-recursive event 'actor 'login)
-		   (assoc-recursive event 'repo 'name)))
-    (url . (format "https://github.com/%s"
-		   (assoc-recursive event 'repo 'name)))))
+  (list
+   (cons 'msg (format "%s created a repository %s"
+		      (assoc-recursive event 'actor 'login)
+		      (assoc-recursive event 'repo 'name)))
+   (cons 'url (format "https://github.com/%s"
+		      (assoc-recursive event 'repo 'name)))))
 
 (defun dgh--parse-fork-event (event)
-  "Parse a ForkCreateEvent data structure from EVENT."
-  '((png . ?????)
-    (msg . (format "%s forked %s from %s"
-		   (assoc-recursive event 'actor 'login)
-		   (assoc-recursive event 'payload 'forkee 'full_name)
-		   (assoc-recursive event 'repo 'name)))
-    (url . (format "https://github.com/%s"
-		   (assoc-recursive event 'repo 'name)))))
+  "Parse a ForkEvent data structure from EVENT."
+  (list
+   (cons 'msg (format "%s forked %s from %s"
+		      (assoc-recursive event 'actor 'login)
+		      (assoc-recursive event 'payload 'forkee 'full_name)
+		      (assoc-recursive event 'repo 'name)))
+    (cons 'url (format "https://github.com/%s"
+		       (assoc-recursive event 'repo 'name)))))
+ 
+(defun dgh--parse-watch-event (event)
+  "Parse a WatchEvent data scructure from EVENT."
+  (list
+   (cons 'msg (format "%s starred %s"
+		      (assoc-recursive event 'actor 'login)
+		      (assoc-recursive event 'repo 'name)))
+   (cons 'url (format "https://github.com/%s"
+		       (assoc-recursive event 'repo 'name)))))
+
+(defun dashboard-insert-github (list-size)
+  "Insert LIST-SIZE amount of events from Github into the dashboard."
+ (dashboard-insert-section
+   "Github:"
+   (dgh--parse-events (dgh--get-events))
+   list-size
+   "g"
+   (lambda (&rest ignore) (browse-url (assoc-default 'url el)))
+   (assoc-default 'msg el)))
 
 (defun assoc-recursive (alist &rest keys)
   "Recursively find (as KEYS) in ALIST."
   (while keys
     (setq alist (cdr (assoc (pop keys) alist))))
   alist)
-
-(dgh--get-events 'dgh--parse-events)
 
 (provide 'emacs-dashboard-github)
 
